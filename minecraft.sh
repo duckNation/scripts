a2585d53d55189926c2522371f62d2b9fc887f6a
haappi <74637670+haappi@users.noreply.github.com>
2022-04-09 12:02:49 -0500

    you need jq lmao

--- minecraft.sh
+++ minecraft.sh
@@ -14,7 +14,7 @@ mkdir server && cd server
 
 sudo apt update && sudo apt upgrade -y
 sudo apt autoremove
-sudo apt install -y openjdk-17-jdk
+sudo apt install -y openjdk-17-jdk jq
 
 sudo apt-get install -y screen
 
